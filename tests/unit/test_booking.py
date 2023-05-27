ac9bb3cd3a43399ba0c0fa62c19b53d865dbbd62
jfgphillips <jfgphillips@gmail.com>
2023-05-27 13:31:17 +0100

    added unit tests for parsers
    added tests for utils/parsers
    
    added tests for utils/parsers

---
+++ tests/unit/test_booking.py
@@ -0,0 +1,9 @@
+import pytest
+import booking
+
+
+@pytest.mark.parametrize("domain", [("gmail.com"), ("yahoo.com"), ("hotmail.com"), ("aol.com"), ("icloud.com")])
+def test_sender_hostname(domain):
+    test_email_address = f"jfgphillips@{domain}"
+    sender = booking.sender()
+    pass
